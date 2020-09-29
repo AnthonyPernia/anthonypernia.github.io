0cf8b1cd2adef768a26fe03d476db06b41a8126b
anthony pernia <anthonyperniah@gmail.com>
2020-09-29 11:57:15 -0300

    add blog part

--- index.js
+++ index.js
@@ -6,16 +6,17 @@ var elements_div = document.getElementsByClassName("card-repo");
 function get_data(element){
     var title = element.getElementsByTagName("h1").item(0);
     var description = element.getElementsByTagName("h2").item(0);
+    var link1 = element.getElementsByTagName("a").item(0);
+    var link2 = element.getElementsByTagName("a").item(1);
     var Http = new XMLHttpRequest();
     var name = title.getAttribute('repo');
     var url='https://api.github.com/repos/anthonyperniah/'+name;
-    console.log(url)
     Http.open("GET", url);
     Http.send();
     Http.onreadystatechange = (e) => {
     var result = JSON.parse(Http.response);
-    console.log(result.name);
-    console.log(result.description);
+    link1.href = result.html_url;
+    link2.href = result.html_url;
     title.innerHTML = result.name;
     description.innerHTML = result.description;
     }
