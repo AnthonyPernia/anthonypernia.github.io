996b51e4cd7311f5ec5255b460665a50d8694e8b
Anthony Pernia <anthonyperniah@gmail.com>
2021-04-09 12:38:47 -0300

    data

--- script/index.js
+++ script/index.js
@@ -1,6 +1,8 @@
 //Script de carga de data
 var elements_div_projects = document.getElementsByClassName("card_container");
-
+var elements_div_blog = document.getElementsByClassName("card_blog");
+var blog_data =
+  "https://drive.google.com/file/d/1p0RHQiNlQgbNle2mtqKC_ibtQSQb-rlJ/view";
 function get_data_projects(element) {
   var title = element.getElementsByTagName("h4").item(0);
   var description = element.getElementsByTagName("p").item(0);
@@ -25,13 +27,37 @@ function get_data_projects(element) {
   };
 }
 
-function set_data(elements_div) {
-  for (var e = 0; e < elements_div.length; e++) {
-    get_data_projects(elements_div[e]);
+function get_data_blog(element) {
+  var title = element.getElementsByTagName("h5").item(0);
+  var body = element.getElementsByTagName("p").item(0);
+  var img = element.getElementsByTagName("img").item(0);
+  var Http = new XMLHttpRequest();
+  Http.open("GET", blog_data);
+  Http.send();
+  Http.onreadystatechange = (e) => {
+    var result = JSON.parse(Http.response);
+    //link1.href = result.html_url;
+    console.log(result);
+    //link2.href = result.html_url;
+    // title.innerHTML = result.name.replace(/([A-Z])/g, " $1").trim();
+    //img.src ="https://raw.githubusercontent.com/AnthonyPerniah/" +result.name +"/master/img_preview/preview.png";
+    // body.innerHTML = result.description;
+  };
+}
+
+function set_data(elements_div_projects, elements_div_blog) {
+  for (var e = 0; e < elements_div_projects.length; e++) {
+    get_data_projects(elements_div_projects[e]);
+  }
+  for (var e = 0; e < elements_div_blog.length; e++) {
+    get_data_blog(elements_div_blog[e]);
   }
 }
 
-document.addEventListener("DOMContentLoaded", set_data(elements_div_projects));
+document.addEventListener(
+  "DOMContentLoaded",
+  set_data(elements_div_projects, elements_div_blog)
+);
 
 //Script de scroll
 
@@ -46,10 +72,3 @@ function scrollFunction() {
 window.onscroll = function () {
   scrollFunction();
 };
-
-
-$(".nav .nav-link").on("click", function(){
-  $(".nav").find(".active").removeClass("active");
-  $(this).addClass("active");
-});
-
