9e7e246237de39f375836399fd270e653e3879b1
Anthony Pernia <anthonyperniah@gmail.com>
2021-04-24 15:25:44 -0300

    img me

--- script/index.js
+++ script/index.js
@@ -1,8 +1,9 @@
 //Script de carga de data
 var elements_div_projects = document.getElementsByClassName("card_container");
 var elements_div_blog = document.getElementsByClassName("card_blog");
+var userGit = "anthonyperniah";
 var blog_data =
-  "https://raw.githubusercontent.com/anthonyperniah/anthonyperniah.github.io/master/data/data.json";
+  "https://raw.githubusercontent.com/"+userGit+"/"+userGit+".github.io/master/data/data.json";
 function get_data_projects(element) {
   var title = element.getElementsByTagName("h4").item(0);
   var description = element.getElementsByTagName("p").item(0);
@@ -10,7 +11,7 @@ function get_data_projects(element) {
   //var link2 = element.getElementsByTagName("a").item(1);
   var img = element.getElementsByTagName("img").item(0);
   var name = title.getAttribute("repo");
-  var url = "https://api.github.com/repos/AnthonyPerniah/" + name;
+  var url = "https://api.github.com/repos/"+userGit+"/" + name;
   var Http = new XMLHttpRequest();
   Http.open("GET", url);
   Http.send();
@@ -20,7 +21,7 @@ function get_data_projects(element) {
     //link2.href = result.html_url;
     title.innerHTML = result.name.replace(/([A-Z])/g, " $1").trim();
     img.src =
-      "https://raw.githubusercontent.com/AnthonyPerniah/" +
+      "https://raw.githubusercontent.com/"+userGit+"/" +
       result.name +
       "/master/img_preview/preview.png";
     description.innerHTML = result.description;
@@ -65,7 +66,6 @@ function set_data(elements_div_projects, elements_div_blog) {
       }
     }
   });
-
 }
 
 document.addEventListener(
