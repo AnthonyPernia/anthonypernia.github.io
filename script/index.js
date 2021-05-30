272991a8f27a12502c012132403d51ff859798f5
Anthony Pernia <anthonyperniah@gmail.com>
2021-05-30 19:53:19 -0300

    prueba post en pdn

--- script/index.js
+++ script/index.js
@@ -3,7 +3,11 @@ var elements_div_projects = document.getElementsByClassName("card_container");
 var elements_div_blog = document.getElementsByClassName("card_blog");
 var userGit = "anthonyperniah";
 var blog_data =
-  "https://raw.githubusercontent.com/"+userGit+"/"+userGit+".github.io/master/data/data.json";
+  "https://raw.githubusercontent.com/" +
+  userGit +
+  "/" +
+  userGit +
+  ".github.io/master/data/data.json";
 function get_data_projects(element) {
   var title = element.getElementsByTagName("h4").item(0);
   var description = element.getElementsByTagName("p").item(0);
@@ -11,7 +15,7 @@ function get_data_projects(element) {
   //var link2 = element.getElementsByTagName("a").item(1);
   var img = element.getElementsByTagName("img").item(0);
   var name = title.getAttribute("repo");
-  var url = "https://api.github.com/repos/"+userGit+"/" + name;
+  var url = "https://api.github.com/repos/" + userGit + "/" + name;
   var Http = new XMLHttpRequest();
   Http.open("GET", url);
   Http.send();
@@ -21,34 +25,33 @@ function get_data_projects(element) {
     //link2.href = result.html_url;
     title.innerHTML = result.name.replace(/([A-Z])/g, " $1").trim();
     img.src =
-      "https://raw.githubusercontent.com/"+userGit+"/" +
+      "https://raw.githubusercontent.com/" +
+      userGit +
+      "/" +
       result.name +
       "/master/img_preview/preview.png";
     description.innerHTML = result.description;
   };
 }
 
-function set_data_blog(element, data) {
-  var title = element.getElementsByTagName("h5").item(0);
-  var body = element.getElementsByTagName("p").item(0);
-  var img = element.getElementsByTagName("img").item(0);
-  var time = element.getElementsByTagName("small").item(0);
-  title.innerHTML = data["title"]
-  body.innerHTML = data["text"]
-  img.src = data["img"]
-  time.innerHTML = "Publicado el : "+data["time"]
-  //var Http = new XMLHttpRequest();
-  //Http.open("GET", blog_data);
-  //Http.send();
-  //Http.onreadystatechange = (e) => {
-    //var result = JSON.parse(Http.response);
-    //link1.href = result.html_url;
-    //console.log(result);
-    //link2.href = result.html_url;
-    // title.innerHTML = result.name.replace(/([A-Z])/g, " $1").trim();
-    //img.src ="https://raw.githubusercontent.com/AnthonyPerniah/" +result.name +"/master/img_preview/preview.png";
-    // body.innerHTML = result.description;
- // };
+function get_data_blogs() {
+  var url = "https://bloganthonyperniah.herokuapp.com/blog/auth/authenticate";
+  var data = new FormData();
+  data.append("username", "anthony");
+  data.append("password", "anthony");
+  var Http = new XMLHttpRequest();
+  Http.open("POST", url, true);
+  Http.onload = function () {
+    // do something to response
+    console.log(this.responseText);
+};
+Http.send(data);
+  
+}
+
+function set_data_blog() {
+  console.log("prueba blog");
+  get_data_blogs();
 }
 
 function set_data(elements_div_projects, elements_div_blog) {
@@ -56,21 +59,12 @@ function set_data(elements_div_projects, elements_div_blog) {
   for (var e = 0; e < elements_div_projects.length; e++) {
     get_data_projects(elements_div_projects[e]);
   }
-  console.log('antes')
-  fetch('../data/data.json')
-  .then(response => response.json())
-  .then(data => {
-    for(var e =0; e < elements_div_blog.length; e++){
-      if(e<=data['blog'].length){
-        set_data_blog(elements_div_blog[e], data['blog'][e])
-      }
-    }
-  });
 }
 
 document.addEventListener(
   "DOMContentLoaded",
-  set_data(elements_div_projects, elements_div_blog)
+  set_data(elements_div_projects, elements_div_blog),
+  set_data_blog()
 );
 
 //Script de scroll
